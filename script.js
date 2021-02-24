00f1bd2da8e99dad86d19d21ad4488d0803492c8
zuraizzafar <zuraiz49@gmail.com>
2021-02-24 19:33:53 +0500

    added popup of the color

--- script.js
+++ script.js
@@ -7,7 +7,33 @@ function spin() {
     spin_button.setAttribute("disabled", "true");
     rotate += random;
     img.setAttribute("style", "transform: rotate(" + rotate + "deg);");
-    setTimeout(function () { 
+    setTimeout(function () {
         spin_button.removeAttribute("disabled");
+        var check = ((rotate - (7200 * flag)) % 360);
+        if (check >= 20 && check <= 66) {
+            alert("Red!");
+        }
+        else if (check >= 67 && check <= 112) {
+            alert("Orange!");
+        }
+        else if (check >= 113 && check <= 156) {
+            alert("Yellow!");
+        }
+        else if (check >= 157 && check <= 199) {
+            alert("Black!");
+        }
+        else if (check >= 200 && check <= 241) {
+            alert("Green!");
+        }
+        else if (check >= 242 && check <= 285) {
+            alert("Blue!");
+        }
+        else if (check >= 286 && check <= 331) {
+            alert("Purple!");
+        }
+        else if (check >= 332 && check <= 379) {
+            alert("Pink!");
+        }
+        ++flag;
     }, 5000);
 }
